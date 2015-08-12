665b70155c4b7507c4ed6d51fe520d8352f4f77c
Arthur Berman <aberman@wr-130-64-93-199.medford.tufts.edu>
2015-08-12 10:42:30 -0400

    implemented multiple players

---
+++ Assets/MPControl.js
@@ -0,0 +1,9 @@
+﻿#pragma strict
+public var player : int = 1;
+function Start () {
+
+}
+
+function Update () {
+    transform.Translate(Vector3(Input.GetAxis("Horizontal"+player), 0, Input.GetAxis("Vertical"+player)));
+}
