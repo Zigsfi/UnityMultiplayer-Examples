e633b745474ad53a91139f8b293afb927afe9211
Thomas Colgrove <Thomas@resnet149-171.medford.tufts.edu>
2015-08-13 16:02:20 -0400

    arthur berman is a g

--- Assets/MPControl.js
+++ Assets/MPControl.js
@@ -6,4 +6,9 @@ function Start () {
 
 function Update () {
     transform.Translate(Vector3(Input.GetAxis("Horizontal"+player), 0, Input.GetAxis("Vertical"+player)));
+    
+    if(Input.GetButton("Fire" + player)) {
+        transform.position = Vector3(Random.Range(1, 10), 0 , Random.Range(1, 10));
+    }
+    
 }
